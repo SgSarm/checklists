e2224b600584017e4e183aea82b970b410c06f0b
sgsarmis <sgsarmis@Stefanos-Gavriils-MacBook-Pro.local>
2017-12-14 16:11:46 +0100

    - add functionality to add and remove items from checklist

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -57,6 +57,7 @@ class ChecklistViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -90,6 +91,13 @@ class ChecklistViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
     
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         if item.checked {
@@ -103,6 +111,21 @@ class ChecklistViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
     }
+    
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        item.checked = true
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
+
     
 }
 
