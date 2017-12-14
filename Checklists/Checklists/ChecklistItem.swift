03a32ae849e1c1261c52bdc354a094535dbac96c
sgsarmis <sgsarmis@Stefanos-Gavriils-MacBook-Pro.local>
2017-12-14 11:29:01 +0100

    add checklistitem model

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Stefanos-Gavriil Sarmis on 14.12.17.
+//  Copyright © 2017 SGSarmis. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
